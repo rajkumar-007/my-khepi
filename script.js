bca7a18d9344fbacf67b3449e937215cbdfe8cee
rajkumar-007 <parbatihansda21@gmail.com>
2026-05-30 22:52:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,49 +5,18 @@ You will always hold a special place in my heart. ❤️
 I hope you'll always remain a part of my life. 💖`;
 const typingElement = document.getElementById("typing");
 if (typingElement) {
-
-    let i = 0;
-
+  let i = 0;
     function typeWriter() {
-
         if (i < text.length) {
-
             if (text.charAt(i) === "\n") {
                 typingElement.innerHTML += "<br>";
             } else {
                 typingElement.innerHTML += text.charAt(i);
             }
-
             i++;
-
             setTimeout(typeWriter, 50);
         }
     }
-
     typeWriter();
 }
 
-// Heart explosion
-document.addEventListener("click", function(e){
-
-    for(let i=0;i<12;i++){
-
-        let heart = document.createElement("div");
-
-        heart.className = "heart";
-
-        heart.innerHTML = "❤️";
-
-        heart.style.left =
-            (e.clientX + Math.random()*100 - 50) + "px";
-
-        heart.style.top =
-            (e.clientY + Math.random()*100 - 50) + "px";
-
-        document.body.appendChild(heart);
-
-        setTimeout(()=>{
-            heart.remove();
-        },2000);
-    }
-});
