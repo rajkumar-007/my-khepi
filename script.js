0b48873ec84ab32357e2f8107156bab42d9cb0ff
rajkumar-007 <parbatihansda21@gmail.com>
2026-05-30 21:19:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
+const text = `To My Khepi 👻,
 
+You make my day brighter with your smile, kindness, and love.
 
+Having you in my life is a blessing, and I cherish every moment we share.
 
+You will always hold a special place in my heart. ❤️
+
+I hope you'll always remain a part of my life. 💖`;
+
+const typingElement = document.getElementById("typing");
+
+if (typingElement) {
+
+    let i = 0;
+
+    function typeWriter() {
+
+        if (i < text.length) {
+
+            if (text.charAt(i) === "\n") {
+                typingElement.innerHTML += "<br>";
+            } else {
+                typingElement.innerHTML += text.charAt(i);
+            }
+
+            i++;
+
+            setTimeout(typeWriter, 50);
+        }
+    }
+
+    typeWriter();
+}
+
+// Heart explosion
 document.addEventListener("click", function(e){
 
     for(let i=0;i<12;i++){
 
         let heart = document.createElement("div");
+
         heart.className = "heart";
+
         heart.innerHTML = "❤️";
 
         heart.style.left =
